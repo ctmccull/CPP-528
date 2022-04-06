0a822bd7eade7daa25422b19aa6561eed9875fbd
radwan-a <70247574+radwan-a@users.noreply.github.com>
2022-04-06 00:25:49 -0500

    lab03 update

---
+++ Utilities.r
@@ -0,0 +1,33 @@
+
+
+# function 1
+filter_cat <- function( data , search )
+  
+{
+  num <- grep(search, data$category, ignore.case = T) 
+  dat.cat <- data.frame(data[num,])
+  return( dat.cat )
+}
+
+# import data dictionary
+dd <- LTDB_DATA_DICTIONARY
+
+# function 2
+search_var <- function(string)
+{
+vector <- dd$definition
+these <- grepl( string, vector, ignore.case=T )
+dat.sub <- dd[ these, ]
+return( dat.sub )
+}
+
+# Function 3 filter by year
+
+filter_year <- function( year )
+{
+  
+  dd.year <- dd %>% dplyr::select( contains( year ))
+  
+  return( dd.year )
+  
+}
